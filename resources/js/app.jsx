8193d7aed88b50b776a688c99a6458d4f3ca6254
Mohammad Salah <83545789+MOSALAH-CODE@users.noreply.github.com>
2023-10-27 12:48:40 +0300

    Code Splitting and Dynamic Imports

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -6,12 +6,6 @@ import {InertiaProgress} from "@inertiajs/progress";
 import Layout from "./Shared/Layout.jsx";
 
 createInertiaApp({
-    // resolve: name => {
-    //     const pages = import.meta.glob('./Pages/**/*.jsx', {eager: true})
-    //     let page = pages[`./Pages/${name}.jsx`]
-    //     page.default.layout = page.default.layout || (page => <Layout children={page}/>)
-    //     return page
-    // },
     resolve: (name) => import(`./Pages/${name}.jsx`).then((module) => {
         const {default: page} = module;
         page.layout = page.layout || ((page) => <Layout children={page}/>);
